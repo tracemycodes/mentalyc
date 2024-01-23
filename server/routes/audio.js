8fe4de24175b2872a52e22704fec72bff66b9b44
Agba Emmanuel <agbaayoh@gmail.com>
2024-01-23 18:47:28 +0100

    feat: save session init upload state

--- server/routes/audio.js
+++ server/routes/audio.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const multer = require('multer');
-const uploadAudio = require('../aws/aws')
+const uploadAudio = require('../aws/aws');
+const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+const SessionSchema = require('../model/session');
+const session = require('../model/session');
 
 // @route     GET api/auth
 // @desc      get all audio recordings
@@ -23,14 +26,53 @@ router.get('/', async (req, res) => {
 // @access    public
 router.post('/', upload.single('audio'), async (req, res) => {
   try {
-    const { name, title, audio } = req.body; // Access other form fields
+    const { name, title } = req.body; // Access other form fields
 
     const audioFile = req.file; // Access the uploaded audio file
 
-    // console.log('audioFile');
-    const link = await uploadAudio(`${name} ${audioFile.fieldname}`, "mentalyc", audioFile.buffer)
+    const audioId = uuidv4();
 
-    res.json({ user: 'user' });
+    const session = new SessionSchema({
+      name: name,
+      title: title,
+      audioName: audioFile.originalname,
+      audioId: audioId,
+      audioDuration: '',
+    });
+
+    const savedSession = await session.save();
+
+    // const savedSession = await session.findById('65aff7462126bf641cd9153d')
+
+    if (savedSession) {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          id: savedSession.id,
+          name: savedSession.name,
+          title: savedSession.title,
+          sessionStatus: savedSession.sessionStatus,
+          uploadStatus: savedSession.uploadStatus,
+          audio: {
+            name: savedSession.audioName,
+            duration: savedSession.audioDuration,
+            id: savedSession.audioId,
+            url: savedSession.audioUrl,
+          },
+        },
+        message: 'Session saved successfully.',
+      });
+    } else {
+      res.status(500).json({
+        status: 'error',
+        error: {
+          message: 'Failed to save the session.',
+          details: 'An error occurred while processing the request.',
+        },
+      });
+    }
+
+    // const link = await uploadAudio(name, audioId, audioFile.buffer)
   } catch (error) {
     res.status(500).send('Server Error');
   }
